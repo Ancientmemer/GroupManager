ee0a6de1691fb38a22552755d096a0975163c338
Ancientmemer <ancientmemer12@gmail.com>
2025-12-30 02:41:25 +0530

    Update admin.py

--- utils/admin.py
+++ utils/admin.py
@@ -1,22 +1,25 @@
-from pyrogram.enums import ChatMemberStatus
+async def is_admin(client, message, chat_id=None):
+    """
+    Checks whether user is admin in a group.
+    - message: original message
+    - chat_id: target group id (optional)
+    """
 
-async def is_admin(client, message):
-    # 1️⃣ Anonymous admin (sender_chat)
+    # PM il aanenkil chat_id must be provided
+    target_chat = chat_id if chat_id else message.chat.id
+
+    # Anonymous admin (sender_chat)
     if message.sender_chat:
         return True
 
-    # 2️⃣ Normal admin
-    if message.from_user:
-        try:
-            member = await client.get_chat_member(
-                message.chat.id,
-                message.from_user.id
-            )
-            return member.status in (
-                ChatMemberStatus.ADMINISTRATOR,
-                ChatMemberStatus.OWNER
-            )
-        except:
-            return False
+    if not message.from_user:
+        return False
 
-    return False
+    try:
+        member = await client.get_chat_member(
+            target_chat,
+            message.from_user.id
+        )
+        return member.status in ("administrator", "owner")
+    except:
+        return False
