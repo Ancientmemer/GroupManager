a3abf564843a4ae65e1b16e4e9be32ed38b8e2b5
Ancientmemer <ancientmemer12@gmail.com>
2025-12-30 03:04:49 +0530

    Update admin.py

--- utils/admin.py
+++ utils/admin.py
@@ -1,14 +1,6 @@
 async def is_admin(client, message, chat_id=None):
-    """
-    Checks whether user is admin in a group.
-    - message: original message
-    - chat_id: target group id (optional)
-    """
-
-    # PM il aanenkil chat_id must be provided
     target_chat = chat_id if chat_id else message.chat.id
 
-    # Anonymous admin (sender_chat)
     if message.sender_chat:
         return True
 
